7319cc52c6f6366801f88f27f6ce3af612aa14f7
Riley Mcintyre <rileysev9@gmail.com>
2025-02-03 15:41:49 +1300

    first commit

---
+++ W1D1Calculator.py
@@ -0,0 +1,48 @@
+while True:
+# Equation template 
+  def Addition(v1 ,v2):
+      return v1 + v2
+
+  def Subtraction(v1 ,v2):
+      return v1 - v2
+
+  def Division(v1 ,v2):
+      return v1 / v2
+
+  def Multiplication (v1 ,v2):
+      return v1 * v2
+
+# Instractions 
+  print(
+      "\n"
+      "+. Addition\n"\
+      "-. Subtraction\n"\
+      "*. Multiplication\n"\
+      "/. Division\n") 
+
+# Valuse
+  v1 =  float(input ("1st Number "))
+  MathSymobles = input ("MathSymobles ")
+  v2 = float(input ("2nd Number "))
+
+# Generates outcome
+  if MathSymobles == "+":
+      (
+  print(Addition(v1,v2))
+      )
+  elif MathSymobles == "-":
+      (
+  print(Subtraction(v1,v2))
+      )
+  elif MathSymobles == "*":
+      (
+  print(Multiplication(v1,v2))
+      ) 
+  elif MathSymobles == "/":
+      (
+  print(Division(v1,v2))
+      )
+  else :
+      (
+         print ("error")
+      ) 
---
+++ W1D1EvenOrOdd.py
@@ -0,0 +1,15 @@
+#Repeat the code
+while True:
+  
+  # Input number for check
+  number = int(input ("Number "))
+
+  # Generates outcome
+  if (number % 2) == 0 :
+      (
+  print ("Even")
+      )
+  else:
+      (
+  print("Odd")
+      )
